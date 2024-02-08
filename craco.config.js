fbb96c506dd4d366e113531b3689b2b149bc2992
Jim Amato <amato1891@yahoo.com>
2024-02-08 00:25:31 -0500

    changes

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,14 @@
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 module.exports = {
-  reactScriptsVersion: "react-scripts",
-  style: {
-    css: {
-      loaderOptions: () => {
-        return {
-          url: false,
-        };
-      },
+  webpack: {
+    configure: (webpackConfig, { env }) => {
+      if (env === 'development') {
+        // Add React Refresh plugin for development builds
+        webpackConfig.plugins.push(new ReactRefreshPlugin());
+      }
+
+      return webpackConfig;
     },
   },
 };
