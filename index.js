42c92f9c615dff8735434df1307a56777d4cf23d
Jim Amato <amato1891@yahoo.com>
2024-02-08 19:14:21 -0500

    fix

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.use(cors());
 
 console.log(`${process.env.NODE_ENV} ENV DETECTED ON SERVER`)
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
