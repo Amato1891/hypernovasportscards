ddb7115e7ca713d6dcb3041efa9baabd1f5fa24d
Jim Amato <amato1891@yahoo.com>
2024-02-08 19:37:35 -0500

    add proxy

--- index.js
+++ index.js
@@ -33,10 +33,6 @@ app.get('/get-token', async (req, res) => {
   }
 });
 
-app.get('/getToken', async (req, res) => { 
-  res.json({ name:'Jim' });
-});
-
 // Define the target URL for the eBay API
 const ebayApiUrl = 'https://api.ebay.com';
 
@@ -46,6 +42,12 @@ const ebayProxy = createProxyMiddleware({
   changeOrigin: true, // Needed for virtual hosted sites
   pathRewrite: {
     '^/api/ebay': '', // Remove '/api/ebay' from the beginning of URL path
+  },
+  onProxyRes: function(proxyRes, req, res) {
+    // Add CORS headers to the response from eBay API
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   },
 });
 
